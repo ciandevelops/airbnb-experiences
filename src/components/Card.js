59013168874a43c90a59908b37e07fb7d43fa473
Cian Butler <ciandevelops@gmail.com>
2022-01-24 11:10:19 -0500

    fixed country to location naming error

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,8 +6,8 @@ const Card = (props) => {
       <div className="card--stats">
         <img src="/images/star.png" alt="star" className="card--star" />
         <span>{props.rating}</span>
-        <span className="gray">{props.reviewCount} • </span>
-        <span className="gray">{props.country}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
       </div>
 
       <p>{props.title}</p>
