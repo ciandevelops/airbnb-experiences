5814ab2cddc02037ac002a6de4c44d380fa8d898
Cian Butler <ciandevelops@gmail.com>
2022-01-24 10:53:02 -0500

    used json to map over cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import Card from "./components/Card";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import data from "./data";
 
 function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount="(6)"
-        country="USA"
-        title="Life Lessons With Katie Zaferes"
-        price={136}
-      />
+      {cards}
     </div>
   );
 }
